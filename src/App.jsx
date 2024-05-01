bfa028a5b434488bfe88bfa40445bbab1ed34108
oldmcdonnell <oldmcdonnell@gmail.com>
2024-05-01 17:28:49 -0400

    fuck this shit i might as well work at home no help NONE

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { useReducer } from 'react'
+//import Container from 'react-bootstrap/Container'
 import './App.css'
-const count = 0
+let count = 0
 
 const Title = () => {
   return (
@@ -43,7 +44,7 @@ const reducer = (state, action) => {
   switch(action.type){
     case 'todoTitleInput':
       console.log('there', action)
-      return checkTodoID(), {todos: [...state.todos, {todoID: count +1 ,todoTitle: action.todoTitle}]};
+      return {todos: [...state.todos, {todoID: state.todos.length + 1, todoTitle: action.todoTitle, todoDescription: action.todoDescription}]};
       // current date should also be here
     case 'todoDescirption':
       return {};
@@ -52,22 +53,42 @@ const reducer = (state, action) => {
   } 
 }
 
+
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState)
   const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
   console.log(state)
 
+  function clearForm(){
+    setTitle(''),
+    setDescription('')
+  }
 
   return (
 <div>
     <Title />
     <input 
+    id='titleBox'
     type="text" 
     value={title}
     onChange={(e)=> setTitle(e.target.value)}>
     </input>
-    <button onClick={() => dispatch({type:"todoTitleInput", todoTitle: title})}>Submit</button>
+    <input 
+    id='decriptionBox'
+    type="text" 
+    value={description}
+    onChange={(e)=> setDescription(e.target.value)}>
+    </input>
+    <button onClick={() => dispatch({type:"todoTitleInput", todoTitle: title, todoDescription: description})}>Submit</button>
     <p></p>
+    {state.todos.map(todo => (
+      <div key={todo.todoID}>
+        Todo: {todo.todoID}: {todo.todoTitle}
+        <br></br>
+        {todo.todoDescription}
+      </div>
+    ))}
   </div>
   )
  }
