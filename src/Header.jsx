f1ae262ee3ec147b6a151abdedfafe44f9a8ee2a
oldmcdonnell <oldmcdonnell@gmail.com>
2024-05-01 12:47:35 -0400

    initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+      <div >
+        ToDo List
+      </div>
+    )
+  }
+  
+  export default Footer
--- src/main.jsx
+++ src/main.jsx
@@ -1,61 +1,68 @@
 import React from 'react'
+import { createContext } from 'react'
+import { useReducer } from 'react'
+import { useState } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
 
 
-// import React, { createContext, useReducer, useState } from 'react'
-// import ReactDOM from 'react-dom/client'
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-//   Outlet,
-// } from 'react-router-dom'
-
-// // project styles
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// import './App.css'
-
-// import App from './App'
-// import Header from './Header'
-// import Footer from './Footer'
-
-// function Layout() {
-//   return (
-//       <>
-//         <Header />
-//         <div id='page-content'>
-//           <Outlet />
-//         </div>
-//         <Footer />
-//       </>
-//   )
-// }
-
-// const router = createBrowserRouter([
-//   {
-//     element: <Layout />,
-//     errorElement: <ErrorPage />,
-//     children: [
-//       {
-//         path: '/',
-//         element: <App />,
-//         errorElement: <ErrorPage />
-//       },
-//     ]
-//   }
-// ])
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+} from 'react-router-dom'
 
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   // <RouterProvider router={router} />
-// <App />
-// )
+// project styles
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 
+
+import App from './App'
+import Header from './Header'
+import Footer from './Footer'
+import ErrorPage from './ErrorPage'
+
+function Layout() {
+  return (
+      <>
+        <Header />
+        <div id='page-content'>
+          <Outlet />
+        </div>
+        <Footer />
+      </>
+  )
+}
+
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <App />,
+        errorElement: <ErrorPage />
+      },
+    ]
+  }
+])
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <RouterProvider router={router} />
+<App />
 )
+
+
+
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
