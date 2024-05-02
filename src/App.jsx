c3ca955d81a5dfa792648991acb986678cfa6ae2
oldmcdonnell <oldmcdonnell@gmail.com>
2024-05-02 16:12:17 -0400

    edit and delete fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import { useReducer } from 'react'
 //import Container from 'react-bootstrap/Container'
 import './App.css'
 let count = 0
+let tempTitle = document.getElementById('titleBox')
+let tempDescription = document.getElementById('decriptionBox')
+
 
 const Title = () => {
   return (
@@ -23,7 +26,7 @@ To-Do List
 export const initialState = () => 
 { 
   let getStore = JSON.parse(localStorage.getItem('todoList'))
-  console.log('storeage in init',getStore)
+  console.log('storage in init',getStore)
   if (getStore) {
     return {todos: getStore}
   } else {
@@ -31,7 +34,10 @@ export const initialState = () =>
   }
 }
 
-
+const setEditFields = () => {
+  tempTitle.value= action.todoTitle
+  tempDescription.value = action.todoDescription
+}
 
 const reducer = (state, action) => {
   switch(action.type){
@@ -41,21 +47,32 @@ const reducer = (state, action) => {
         todoTitle: action.todoTitle, 
         todoDescription: action.todoDescription}]};
       // current date should also be here
-    case 'todoDescirption':
-      return {};
-    case 'todoDue':
-      return {}
+    case 'todoEdit':
+       console.log('todoID', action.todoID, action.todoTitle, action.todoDescription);
+       return {todos: [...state.todos.map(el => {
+        if (action.editID === el.todoID){
+          el.todoTitle = action.todoTitle 
+          el.todoDescription = action.todoDescription
+        }
+        return el
+       })]}
+    case 'todoDelete':
+      console.log('todoID', action.todoID, action.todoTitle, action.todoDescription);
+      return {todos: [...state.todos.filter(el => el.todoID !== action.todoID)]} 
   } 
 }
 
 
 
 function App() {
-  //let temp = initialState()
+  const [edit, setEdit] = useState(false);
+  const [editID, setEditID] = useState(0);
   const [state, dispatch] = useReducer(reducer, initialState())
   const [title, setTitle] = useState('')
   const [description, setDescription] = useState('')
-  console.log(state.todos)
+  const [idForTodo, setIdForTodo] = useState('')
+
+  //console.log(state.todos)
 
   
   function clearForm(){
@@ -65,10 +82,12 @@ function App() {
 
   useEffect(() => {
     localStorage.setItem('todoList', JSON.stringify(state.todos))
-  }, [state.todos.length])
+    clearForm()
+    setEdit(false)
+    setEditID(0)
+  }, [state.todos])
   
-  // let getStore = JSON.parse(localStorage.getItem('todoList'))
-  // console.log('storeage test',getStore)
+
 
   return (
 <div>
@@ -85,17 +104,25 @@ function App() {
     value={description}
     onChange={(e)=> setDescription(e.target.value)}>
     </input>
-    <button onClick={() => dispatch({type:"todoTitleInput", todoTitle: title, todoDescription: description})}>Submit</button>
+    <button onClick={() => dispatch({type: edit ? "todoEdit" : "todoTitleInput", editID: editID, todoTitle: title, todoDescription: description})}>{edit ? "Save" : "Submit"}</button>
     <p></p>
     {state.todos.map(todo => (
       <div key={todo.todoID}>
         Todo: {todo.todoID}: {todo.todoTitle}
         <br></br>
         {todo.todoDescription}
+        <br></br>
+        <button key={todo.todoID} onClick={() => {setTitle(todo.todoTitle); setDescription(todo.todoDescription); setEditID(todo.todoID); setEdit(true)}}>Edit</button>
+          <br></br>
+        <button key={todo.todoTitle} onClick={() => dispatch({type:"todoDelete", todoID:todo.todoID, 
+        todoTitle:todo.todoDescription, todoDescription:todo.todoDescription})}>Delete</button>
       </div>
     ))}
   </div>
   )
  }
+
+
+
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -53,8 +53,7 @@ const router = createBrowserRouter([
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <RouterProvider router={router} />
-<App />
+  <RouterProvider router={router} />
 )
 
 // ReactDOM.createRoot(document.getElementById('root')).render(
